538cf16fb7e6c1435188bc03c7826500c70def2f
Nam Dang <dangquangnam75@gmail.com>
2021-09-08 23:22:57 -0500

    Subtracting numbers

--- TwoSum.py
+++ TwoSum.py
@@ -1,14 +1,27 @@
-nums = [3, 2, 4]
-target = 6
+nums = [0,4,3,0]
+target = 0
 
-sum = 0
 needed_idx = []
+hash_map = {}
 
-# O(n(k-1))
+# Subtracted from target
 for i in range(len(nums)):
-    for j in range(i + 1, len(nums)):
-        if nums[i] + nums[j] == target:
-            needed_idx.append(i)
-            needed_idx.append(j)
+    diff = target - nums[i]
+    if i == 0:
+        hash_map[0] = nums[0]
+    elif diff in hash_map.values():
+        needed_idx.append(i)
+        needed_idx.append(list(hash_map.keys())[list(hash_map.values()).index(diff)])
+    else:
+        hash_map[i] = nums[i]
 
-print(f"The answer is {needed_idx}")
+print(needed_idx)
+
+# O(n**2)
+# for i in range(len(nums)):
+#     for j in range(i + 1, len(nums)):
+#         if nums[i] + nums[j] == target:
+#             needed_idx.append(i)
+#             needed_idx.append(j)
+#
+# print(f"The answer is {needed_idx}")
