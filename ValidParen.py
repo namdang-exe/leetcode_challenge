62bf98823fe4d3e838be40952235efb2b8a48dcb
Nam Dang <dangquangnam75@gmail.com>
2021-09-15 23:51:35 -0500

    Valid Parentheses working solution

--- ValidParen.py
+++ ValidParen.py
@@ -1,27 +1,33 @@
-s = "{[]}"
-
-hash_map = {
-    ')': '(', ']': '[', '}': '{'
-}
-
-
 def get_key(val):
     for key, value in hash_map.items():
         if val == value:
             return key
 
 
+s = "(){}}{"
+hash_map = {
+    ')': '(', ']': '[', '}': '{'
+}
+paren = '(', '[', '{'
 s = list(s)
 # Pop until the list is empty
-# But it doesn't detect the wrong order
 i = 0
-while s and i <= len(s):
-    if get_key(s[0]) in s:
-        s.remove(get_key(s[0]))
-        s.pop(0)
+empty_list = []
+while i < len(s):
+    if s[i] in paren:
+        empty_list.append(s[i])
+    else:
+        # empty list can't bbe emptied
+        if len(empty_list) == 0:
+            break
+        else:
+            if hash_map.get(s[i]) == empty_list[-1]:
+                empty_list.pop()
+            else:
+                break
     i += 1
 
-if len(s) == 0:
+if len(empty_list) == 0:
     print("Valid")
 else:
     print("Invalid")
