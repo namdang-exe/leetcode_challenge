02f45fd0de789d495dbe9051cd27e48e89700693
Nam Dang <dangquangnam75@gmail.com>
2021-09-18 22:19:02 -0500

    Climbing stairs brute force recursive sol

---
+++ ClimbingStairs.py
@@ -0,0 +1,12 @@
+n = 2
+
+
+def climb_stairs(i, n):
+    if i > n:
+        return 0
+    if i == n:
+        return 1
+    return climb_stairs(i + 1, n) + climb_stairs(i + 2, n)
+
+
+print(climb_stairs(0, 38))
