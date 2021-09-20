ff0c24d5909bd2f6905f4b486635214af6742289
Nam Dang <dangquangnam75@gmail.com>
2021-09-20 18:18:23 -0500

    Climbing Stair dynamic programming sol

--- ClimbingStairs.py
+++ ClimbingStairs.py
@@ -1,12 +1,10 @@
-n = 2
+n = 5
 
-# Brute force recufsive solution
-def climb_stairs(i, n):
-    if i > n:
-        return 0
-    if i == n:
-        return 1
-    return climb_stairs(i + 1, n) + climb_stairs(i + 2, n)
+# Dynamic Programming
+one, two = 1, 1
+for i in range(n - 1):
+    temp = one
+    one = one + two
+    two = temp
 
-
-print(climb_stairs(0, 38))
+print(one)
