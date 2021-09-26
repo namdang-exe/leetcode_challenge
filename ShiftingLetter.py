c97670502a82f052e5eda8e6624e534c2e3fe4ae
Nam Dang <dangquangnam75@gmail.com>
2021-09-26 13:37:18 -0500

    Shifting letter brute force

---
+++ ShiftingLetter.py
@@ -0,0 +1,13 @@
+# Brute force
+from typing import List
+
+
+def shiftingLetters(self, s: str, shifts: List[int]) -> str:
+    def shiftLet(s, n):
+        return chr((ord(s) + n - 97) % 26 + 97)
+
+    s = list(s)
+    for i in range(len(s)):
+        for j in range(i + 1):
+            s[j] = shiftLet(s[j], shifts[i])
+    return ''.join(s)
