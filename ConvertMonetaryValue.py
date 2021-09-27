2f886751404aec2efed5a1a51ae27e028ce53c7c
Nam Dang <dangquangnam75@gmail.com>
2021-09-27 16:23:03 -0500

    Convert the number of the money to words

---
+++ ConvertMonetaryValue.py
@@ -0,0 +1,51 @@
+def convertMonetaryValue(s):
+    hash_map = {
+        1: "one",
+        2: "two",
+        3: "three",
+        4: 'four',
+        5: 'five',
+        6: 'six',
+        7: 'seven',
+        8: 'eight',
+        9: 'nine',
+        10: 'ten',
+        11: 'eleven',
+        12: 'twelve',
+        13: 'thirteen',
+        14: 'fourteen',
+        15: 'fifteen',
+        16: 'sixteen',
+        17: 'seventeen',
+        18: 'eighteen',
+        19: 'nineteen',
+        20: 'twenty',
+        30: 'thirty',
+        40: 'forty',
+        50: 'fifty',
+        60: 'sixty',
+        70: 'seventy',
+        80: 'eighty',
+        90: 'ninety'
+    }
+
+    def convert(num):
+        # 11.23
+        # 11 => eleven
+        if 0 <= num <= 19:
+            return hash_map.get(int(num))
+        # 20 - 99
+        else:
+            tenth, remainder = divmod(num, 10)
+            tenth = str(tenth) + '0'
+            return hash_map.get(int(tenth)) + " " + hash_map.get(remainder)
+
+    s = s[1:]
+    one, two = s.split(".")
+    one = convert(int(one))
+    two = convert(int(two))
+    return f"{one} dollars and {two} cents."
+
+
+
+print(convertMonetaryValue("$11.23"))
