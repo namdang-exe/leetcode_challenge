2f2273cd0111c3a6c36a0a012faa167f5d944065
Nam Dang <dangquangnam75@gmail.com>
2021-09-29 21:16:47 -0500

    Optimized solution for defuse bomb

--- Day3/DefuseBomb.py
+++ Day3/DefuseBomb.py
@@ -1,8 +1,10 @@
-# TODO: Optimized initialization
 def defuseBomb(nums, s, k):
     out = []
     # Initialize sum
-    cur_sum = sum(nums[1:k + 1])
+    # cur_sum = sum(nums[1:k + 1])
+    cur_sum = 0
+    for i in range(1, k + 1):
+        cur_sum += nums[i % s]
     out.append(cur_sum)
     for i in range(1, s):
         cur_sum = cur_sum + nums[(i + k) % s] - nums[i]
