e63f8eaccf0612729a48921f124715fb18c40a32
Nam Dang <dangquangnam75@gmail.com>
2021-10-05 15:42:23 -0500

    Maximum Array Linear Sol O(n)

--- MaximumSubarray.py
+++ MaximumSubarray.py
@@ -1,11 +1,9 @@
-# A little more efficient O(n^2) sol
+# Linear Solution O(n)
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        max_sum = float('-inf')
-        for i in range(len(nums)):
-            cur_sum = 0
-            for j in range(i, len(nums)):
-                cur_sum += nums[j]
-                max_sum = max(max_sum, cur_sum)
+        max_sum = cur_sum = nums[0]
+        for num in nums[1:]:
+            cur_sum += num
+            cur_sum = max(cur_sum, num)
+            max_sum = max(max_sum, cur_sum)
         return max_sum
-                
