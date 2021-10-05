a9c969acd1f44c5763a61eded7cce9c3935d25f4
Nam Dang <dangquangnam75@gmail.com>
2021-10-05 14:30:37 -0500

    Maximum Subarray Brute Force O(n^3) sol

--- MaximumSubarray.py
+++ MaximumSubarray.py
@@ -1,18 +1,10 @@
-from typing import List
-
-
+# Brute force O(n*3) sol
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        cur_sum = max_sum = nums[0]
-        start = 0
-        for i in range(1, len(nums)):
-            cur_sum = cur_sum + nums[i]
-            if cur_sum < cur_sum - nums[start]:
-                cur_sum = cur_sum - nums[start]
-                start += 1
-            else:
-                if nums[i] > cur_sum:
-                    cur_sum = nums[i]
-                    start = i
-            max_sum = max(max_sum, cur_sum)
-        return max_sum
+        max_subarr = float('-inf')
+        for i in range(len(nums)):
+            # nums[0:0] = [] --- nums[0:1] = [-2]
+            for j in range(i,len(nums)):
+                max_subarr = max(max_subarr, sum(nums[i:j+1]))
+        return max_subarr
+                
