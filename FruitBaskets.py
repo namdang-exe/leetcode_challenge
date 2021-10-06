73feb7a5aebdfc9c7a8d6018905197b36751e535
Nam Dang <dangquangnam75@gmail.com>
2021-10-06 11:00:55 -0500

    Fruit into Baskets O(n) time O(1) space

--- FruitBaskets.py
+++ FruitBaskets.py
@@ -1,11 +1,12 @@
-# Sliding window O(n) time and O(n) space sol
+# Sliding window O(n) time and O(k) space sol
+# since k = 2 => O(1) space complexity
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
         max_len = float('-inf')
         unique_f = dict()
         start = 0
         for end, fruit in enumerate(fruits):
-            unique_f[fruit] = unique_f[fruit] + 1 if unique_f.get(fruit) else 1
+            unique_f[fruit] = unique_f.get(fruit, 0) + 1
             # check for unique fruit in the dict
             while len(unique_f) > 2:
                 unique_f[fruits[start]] -= 1
@@ -14,4 +15,3 @@ class Solution:
                 start += 1
             max_len = max(max_len, end - start + 1)
         return max_len
-                
