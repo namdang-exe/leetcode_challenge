a28d99078a566351775283122195611514c493ad
Nam Dang <dangquangnam75@gmail.com>
2021-10-12 16:17:44 -0500

    Guess Num Higher or Lower Bin Search O(lgN)

--- GuessNumber.py
+++ GuessNumber.py
@@ -1,13 +1,19 @@
-# Brute force
-
 # The guess API is already defined for you.
 # @param num, your guess
 # @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
 # def guess(num: int) -> int:
 
+# Bin search
 class Solution:
     def guessNumber(self, n: int) -> int:
-        for i in range(1,n+1):
-            res = guess(i)
+        lo, hi = 1, n
+        while lo <= hi:
+            # round down
+            mid = (lo + hi)//2
+            res = guess(mid)
             if res == 0:
-                return i
+                return mid
+            elif res == -1:
+                hi = mid - 1
+            elif res == 1:
+                lo = mid + 1
