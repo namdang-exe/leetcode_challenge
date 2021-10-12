1925417e10a1b6ea59a5167f5d283313368cf485
Nam Dang <dangquangnam75@gmail.com>
2021-10-12 14:55:51 -0500

    Fibonacci Seq Recursive w Memoise O(n) sol

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,9 +1,14 @@
-# Recursive fibonacci
+# Recursive with Memoise
 class Solution:
     def fib(self, n: int) -> int:
-        if n == 0:
-            return 0
-        if n == 1:
-            return 1
-        else:
-            return self.fib(n-1) + self.fib(n-2)
+        cache = {}
+        def fibSeq(cache, n):
+            if cache.get(n) is not None:
+                return cache[n]
+            if n == 0: return 0
+            if n == 1: return 1
+            else: 
+                res = fibSeq(cache, n-1) + fibSeq(cache, n-2)
+                cache[n] = res
+                return res
+        return fibSeq(cache, n)
