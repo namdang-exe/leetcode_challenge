04e7684480d80b22ce3956f9a674492dc3ed8030
Nam Dang <dangquangnam75@gmail.com>
2021-10-12 09:20:52 -0500

    Contains Duplicate convert to set O(n) sol

---
+++ ContainsDuplicate.py
@@ -0,0 +1,7 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        set_nums = set(nums)
+        if len(set_nums) == len(nums):
+            return False
+        else:
+            return True
