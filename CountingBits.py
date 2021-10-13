3459dbdc03a7085690839607fb7aff17c36372ff
Nam Dang <dangquangnam75@gmail.com>
2021-10-13 11:26:25 -0500

    Counting Bits Math Approach O(nlgn)

--- CountingBits.py
+++ CountingBits.py
@@ -1,10 +1,14 @@
-# Brute Force O(n^2)
+# Brute Force O(nlgn), pure Math approach
 
 class Solution:
     def countBits(self, n: int) -> List[int]:
         res = []
         for i in range(n+1):
-            binary = bin(i)
-            cnt = Counter(binary)
-            res.append(cnt['1'])
+            # use pure math
+            cnt = 0
+            while i != 0:
+                if i % 2 == 1:
+                    cnt += 1
+                i //= 2
+            res.append(cnt)
         return res
