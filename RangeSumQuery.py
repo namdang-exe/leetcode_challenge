04f160fc670b09f510bd04df12113360080d3c32
Nam Dang <dangquangnam75@gmail.com>
2021-10-13 10:04:56 -0500

    Range Sum Query Cum Sum O(1) time, O(n) space

--- RangeSumQuery.py
+++ RangeSumQuery.py
@@ -1,14 +1,21 @@
 class NumArray:
 
     def __init__(self, nums: List[int]):
-        self.nums = nums
-        
+        self.cum = [0] + list(accumulate(nums))
 
     def sumRange(self, left: int, right: int) -> int:
-        res = sum(self.nums[left:right+1])
-        return res
+        return self.cum[right+1] - self.cum[left]
 
 
 # Your NumArray object will be instantiated and called as such:
 # obj = NumArray(nums)
 # param_1 = obj.sumRange(left,right)
+
+# Dynamic Programming
+
+# [-2, 0, 3, -5, 2, -1]
+#   0,-2,-2,1, -4, -2, -3
+
+# if we want the sum range of [2:5] or [3,-5,2]
+# we can calculate by using the sum of 
+# [0:5] - [0:2] or sum([-2,0,3,-5,2]) - sum([-2,0,3])
