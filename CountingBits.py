0a0e6995f85d013743fcd5a5955b6d6741da46a5
Nam Dang <dangquangnam75@gmail.com>
2021-10-13 11:30:40 -0500

    Counting Bits, Math Approach O(nlogn)

--- CountingBits.py
+++ CountingBits.py
@@ -1,11 +1,12 @@
-# Brute Force O(nlgn), pure Math approach
+# Brute Force O(nlogn), Math approach
 
 class Solution:
     def countBits(self, n: int) -> List[int]:
         res = []
         for i in range(n+1):
-            # use pure math
+            # use math
             cnt = 0
+            # this take O(logn) because we can only divide an integer logn times
             while i != 0:
                 if i % 2 == 1:
                     cnt += 1
