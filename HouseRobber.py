6ccfaca6945077475acb36c6e14ce6f0f5ef9862
Nam Dang <dangquangnam75@gmail.com>
2021-10-18 16:30:54 -0500

    House Robber DP O(n) time O(1) space

--- HouseRobber.py
+++ HouseRobber.py
@@ -1,15 +1,9 @@
-# Dynamic Programming O(n) time
+# Dynamic Programming O(n) time O(1) space
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        n = len(nums)
-        # edge cases
-        if n == 1:
-            return nums[0]
-        dp = [0] * n
-        dp[0] = nums[0]
-        dp[1] = max(nums[1], dp[0])
-        if n == 2:
-            return dp[-1]
-        for i in range(1, n):
-            dp[i] = max(nums[i], dp[i-1], dp[i-2]+nums[i])
-        return dp[-1]
+        one, two = 0, 0
+        for n in nums:
+            new_rob = max(one + n, two)
+            one = two
+            two = new_rob
+        return two
