8b971228e19869f071688576f6aa4d3dc339bcf1
Nam Dang <dangquangnam75@gmail.com>
2021-10-21 15:47:56 -0500

    Coin Change II Recursion w Memoize O(S*n) time

--- CoinChangeII.py
+++ CoinChangeII.py
@@ -1,7 +1,10 @@
-# Recursion O(amount**n) time TLE
+# Recursion with Memoize O(amount*n)
 
 
 class Solution:
+    def __init__(self):
+        self.mem = {}
+        
     def change(self, amount: int, coins: List[int]) -> int:
         coins = sorted(coins)
         n = len(coins)
@@ -16,10 +19,14 @@ class Solution:
         #   2. Subproblem that does not contain the last coin, the remain stays the same
         #   E.g. 5,[1,2]
         
+        # Memoize
+        if self.mem.get((remain, n)) is not None:
+            return self.mem[(remain,n)]
         # base cases
         if remain == 0:
             return 1
         if remain < 0 or n == 0:
             return 0
         res = self.recursion(remain - coins[n-1], coins, n) + self.recursion(remain, coins, n-1)
+        self.mem[(remain, n)] = res
         return res
