5b593e7468fab205d77f967e65764439095da8fb
Nam Dang <dangquangnam75@gmail.com>
2021-10-28 14:49:44 -0500

    DFS Binary Tree iterative and recursive

--- DfsBinTree.py
+++ DfsBinTree.py
@@ -5,19 +5,29 @@
 #     self.right = None
 
 def depth_first_values(root):
-  # dfs iterative
-  # using stack
-  # last in first out
-  if root is None:
-    return []
-  result = []
-  stack = [root]
-  while stack:
-    current = stack.pop()
-    result.append(current.val)
+  # dfs recursive
+  if root is None: return []
+  # a
+  left = depth_first_values(root.left) # [b, d, e]
+  right = depth_first_values(root.right) # [c,f]
+  return [root.val] + left + right
+
+# def depth_first_values(root):
+#   # dfs iterative
+#   # using stack
+#   # last in first out
+#   if root is None:
+#     return []
+#   result = []
+#   stack = [root]
+#   while stack:
+#     current = stack.pop()
+#     result.append(current.val)
     
-    # left and right node
-    if current.right is not None: stack += [current.right]
-    if current.left is not None: stack += [current.left]
+#     # left and right node
+#     if current.right is not None: stack += [current.right]
+#     if current.left is not None: stack += [current.left]
     
-  return result
+#   return result
+
+
