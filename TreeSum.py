f64d9b8a41ed51d4dbc6585ac793a986d647ac74
Nam Dang <dangquangnam75@gmail.com>
2021-10-28 18:55:54 -0500

    Tree Sum DFS iterative and recursive

--- TreeSum.py
+++ TreeSum.py
@@ -5,9 +5,26 @@
 #     self.right = None
 
 def tree_sum(root):
-  # dfs recursive
+  # dfs iterative
+  # stack
+
   if root is None: return 0
+  total = 0
+  stack = [root]
+  while stack:
+    current = stack.pop()
+    total += current.val
+    
+    # left and right node
+    if current.left is not None: stack.append(current.left)
+    if current.right is not None: stack.append(current.right)
+    
+  return total
+
+# def tree_sum(root):
+#   # dfs recursive
+#   if root is None: return 0
   
-  left = tree_sum(root.left)
-  right = tree_sum(root.right)
-  return root.val + left + right
+#   left = tree_sum(root.left)
+#   right = tree_sum(root.right)
+#   return root.val + left + right
