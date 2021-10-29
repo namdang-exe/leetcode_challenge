fe9344a37a95e21c66fad517d8fc03c39f38ac1f
Nam Dang <dangquangnam75@gmail.com>
2021-10-29 11:03:16 -0500

    Min Depth DFS iterative + recursive

--- MinDepth.py
+++ MinDepth.py
@@ -6,8 +6,7 @@
 #         self.right = right
 class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
-        # dfs iterative
-        # stack
+        # dfs recursive
         
         if root is None: return 0
         
@@ -16,3 +15,24 @@ class Solution:
             return max(self.minDepth(root.left), self.minDepth(root.right)) + 1
         else:
             return min(self.minDepth(root.left), self.minDepth(root.right)) + 1
+        
+   def minDepth(self, root: Optional[TreeNode]) -> int:
+    # dfs iterative
+    # stack
+    if root is None:
+        return 0
+    min_tracker = float('inf')
+    stack = [[root, 1]]
+    while stack:
+        current = stack.pop()
+        cur_node, cur_depth = current
+
+        # Update min tracker if it is a leaf
+        if cur_node.left is None and cur_node.right is None: # a leaf
+            min_tracker = min(min_tracker, cur_depth)
+
+        # left and right node
+        if cur_node.left is not None: stack.append([cur_node.left, cur_depth + 1])
+        if cur_node.right is not None: stack.append([cur_node.right, cur_depth + 1])
+
+    return min_tracker
