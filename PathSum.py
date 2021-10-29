5f8ff1fd21181f9ade561fe66f48cde92e233b0e
Nam Dang <dangquangnam75@gmail.com>
2021-10-29 14:59:02 -0500

    Path Sum DFS Recursive

--- PathSum.py
+++ PathSum.py
@@ -6,24 +6,14 @@
 #         self.right = right
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
-        # dfs iterative
-        # stack
-        # last in first out
-        if not root:
-            return False
+        # dfs recursive
         
-        # [root, 5]
-        stack = [[root, root.val]]
-        while stack:
-            current = stack.pop()
-            cur_node, cur_sum = current
-            # if it's a leaf, then compare w target
-            if not cur_node.left and not cur_node.right:
-                if cur_sum == targetSum:
-                    return True
-            
-            # left and right node
-            if cur_node.left: stack.append([cur_node.left, cur_sum + cur_node.left.val])
-            if cur_node.right: stack.append([cur_node.right, cur_sum + cur_node.right.val])                
-                
-        return False
+        # base case
+        if not root: return False
+        
+        targetSum -= root.val
+        # if it's a leaf, check if the diff == 9
+        if not root.left and not root.right:
+            return targetSum == 0
+        return self.hasPathSum(root.left, targetSum) or self.hasPathSum(root.right, targetSum)
+        
