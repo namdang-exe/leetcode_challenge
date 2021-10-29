156d9ac869e668a61eb185031923a21b419a801e
Nam Dang <dangquangnam75@gmail.com>
2021-10-28 19:12:30 -0500

    Tree Min Value DFS recursive and iterative

--- TreeMinVal.py
+++ TreeMinVal.py
@@ -5,9 +5,24 @@
 #     self.right = None
 
 def tree_min_value(root):
-  # dfs recursive
-  if root is None: return float('inf')
-  # left and right node
-  left = tree_min_value(root.left)
-  right = tree_min_value(root.right)
-  return min(root.val, left, right)v
+  # dfs iterative
+  # stack
+  min_val = float('inf')
+  stack = [root]
+  while stack:
+    current = stack.pop()
+    min_val = min(min_val, current.val)
+    
+    # left and right node
+    if current.left is not None: stack.append(current.left)
+    if current.right is not None: stack.append(current.right)
+  return min_val
+    
+
+# def tree_min_value(root):
+#   # dfs recursive
+#   if root is None: return float('inf')
+#   # left and right node
+#   left = tree_min_value(root.left)
+#   right = tree_min_value(root.right)
+#   return min(root.val, left, right)
