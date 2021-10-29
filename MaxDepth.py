0a6b00c15638157389ff0208b7b0840eda614c4f
Nam Dang <dangquangnam75@gmail.com>
2021-10-29 11:35:23 -0500

    Max Depth DFS iterative + recursive Linear Time

--- MaxDepth.py
+++ MaxDepth.py
@@ -15,3 +15,25 @@ class Solution:
         left = self.maxDepth(root.left)
         right = self.maxDepth(root.right)
         return 1 + max(left, right)
+
+    
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+    # dfs iterative
+    # stack
+    # last in first out
+
+    if root is None: return 0
+    max_tracker = float('-inf')
+    stack = [[root, 1]]
+    while stack:
+        current = stack.pop()
+        cur_node, cur_depth = current
+        if cur_node.left is None and cur_node.right is None: # a leaf
+            max_tracker = max(max_tracker, cur_depth)
+
+        # left and right node
+        if cur_node.left is not None: stack.append([cur_node.left, cur_depth + 1])
+        if cur_node.right is not None: stack.append([cur_node.right, cur_depth + 1])
+
+
+    return max_tracker
