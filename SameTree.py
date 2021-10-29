1b19015765c89f301763a8a3296ded474b2abe4c
Nam Dang <dangquangnam75@gmail.com>
2021-10-29 14:31:34 -0500

    Same Tree Nice Recursive solution

--- SameTree.py
+++ SameTree.py
@@ -4,38 +4,18 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-from collections import deque
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        # bfs iterative
-        # queue
+        # recursive
         
+        # base case
+        # if they both empty => Same
         if not p and not q:
             return True
+        # if one of them is empty => Not the same
         if not p or not q:
             return False
-        
-        qu = deque([[p,q], ])
-        while qu:
-            current = qu.pop()
-            cur_p, cur_q = current                   
-
-            if cur_p.val != cur_q.val:
-                return False
-            
-            # left and right node:
-            if not cur_p.left and cur_q.left:
-                return False
-            if not cur_p.right and cur_q.right:
-                return False
-            if cur_p.left and not cur_q.left:
-                return False
-            if cur_p.right and not cur_q.right:
-                return False
-            
-            if cur_p.left and cur_q.left: 
-                qu.appendleft([cur_p.left, cur_q.left])
-            if cur_p.right and cur_q.right: 
-                qu.appendleft([cur_p.right, cur_q.right])
-
-        return True
+        # their val is not == => not the same
+        if p.val != q.val:
+            return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
