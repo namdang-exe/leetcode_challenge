49005ae89be8c49027b39c3637b719556053434c
Nam Dang <dangquangnam75@gmail.com>
2021-10-29 11:36:30 -0500

    Max Depth DFS Iterative + Recursive changes

--- MaxDepth.py
+++ MaxDepth.py
@@ -18,22 +18,22 @@ class Solution:
 
     
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-    # dfs iterative
-    # stack
-    # last in first out
+        # dfs iterative
+        # stack
+        # last in first out
 
-    if root is None: return 0
-    max_tracker = float('-inf')
-    stack = [[root, 1]]
-    while stack:
-        current = stack.pop()
-        cur_node, cur_depth = current
-        if cur_node.left is None and cur_node.right is None: # a leaf
-            max_tracker = max(max_tracker, cur_depth)
+        if root is None: return 0
+        max_tracker = float('-inf')
+        stack = [[root, 1]]
+        while stack:
+            current = stack.pop()
+            cur_node, cur_depth = current
+            if cur_node.left is None and cur_node.right is None: # a leaf
+                max_tracker = max(max_tracker, cur_depth)
 
-        # left and right node
-        if cur_node.left is not None: stack.append([cur_node.left, cur_depth + 1])
-        if cur_node.right is not None: stack.append([cur_node.right, cur_depth + 1])
+            # left and right node
+            if cur_node.left is not None: stack.append([cur_node.left, cur_depth + 1])
+            if cur_node.right is not None: stack.append([cur_node.right, cur_depth + 1])
 
 
-    return max_tracker
+        return max_tracker
