256b149263070e821bc142fa7bb8a7f7721d5f7e
Nam Dang <dangquangnam75@gmail.com>
2021-10-29 10:43:44 -0500

    Min Depth of Bin Tree DFS recursive

---
+++ MinDepth.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        # dfs iterative
+        # stack
+        
+        if root is None: return 0
+        
+        children = [root.left, root.right]
+        if None in children:
+            return max(self.minDepth(root.left), self.minDepth(root.right)) + 1
+        else:
+            return min(self.minDepth(root.left), self.minDepth(root.right)) + 1
