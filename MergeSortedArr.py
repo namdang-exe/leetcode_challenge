b4f506073d50d38a94dac9a2b785a9304c727f71
Nam Dang <dangquangnam75@gmail.com>
2021-11-01 13:35:40 -0500

    Merge Sorted Array Merge and Sort

--- MergeSortedArr.py
+++ MergeSortedArr.py
@@ -3,21 +3,9 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        # three pointers
-        i, j = 0, 0
-        while i < m and j < n:
-            if nums1[i] <= nums2[j]:
-                i += 1
-            else:
-                nums1.insert(i, nums2[j])
-                nums1.pop()
-                j += 1
-                i += 1
-                m += 1
-        while j < n:
-            nums1[i] = nums2[j]
-            j += 1
-            i += 1
+        
+        nums1[m:] = nums2
+        nums1.sort()    
         
             
         
