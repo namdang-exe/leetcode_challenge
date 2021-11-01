0bbbe94bb296e6496c9d65c12038299c6631472e
Nam Dang <dangquangnam75@gmail.com>
2021-11-01 14:55:16 -0500

    Merge Two Sorted Lists Recursion

--- MergeTwoSortedList.py
+++ MergeTwoSortedList.py
@@ -1,32 +1,19 @@
 # Definition for singly-linked list.
-from typing import Optional
-
-l1 = [1, 2, 4]
-l2 = [1, 3, 4]
-
-
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
-
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode()
-        tail = dummy
-        while l1 and l2:
-            if l1.val > l2.val:
-                tail.next = l2
-                l2 = l2.next
-            else:
-                tail.next = l1
-                l1 = l1.next
-            tail = tail.next
-
-        if l1:
-            tail.next = l1
-        elif l2:
-            tail.next = l2
-        return dummy.next
-
+        # recursive
+        
+        # base case
+        if l1 is None: return l2
+        if l2 is None: return l1
+        
+        if l1.val <= l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
