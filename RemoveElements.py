e6bbfb37db11f71efd167f24ba715ba0c0b2696e
Nam Dang <dangquangnam75@gmail.com>
2021-11-03 14:16:10 -0500

    Remove Elements Recursion

---
+++ RemoveElements.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        # Recursion
+        
+        # base case
+        if head is None: return None
+        
+        # if the current node is the supposedly-deleted node, make it the next node
+        if head.val == val:
+            head = self.removeElements(head.next, val)
+        else:
+            head.next = self.removeElements(head.next, val)
+            
+        return head
