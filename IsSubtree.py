17a5b6f196e3628cb3473870e3f3f38b7d4cdd03
Nam Dang <dangquangnam75@gmail.com>
2021-11-05 11:53:10 -0500

    Subtree of Another Tree Recursion

--- IsSubtree.py
+++ IsSubtree.py
@@ -26,32 +26,37 @@ class Solution:
         # the right node with subroot's right node
         # => they have to be equal to return True
         
-        # isMatch() will compare subtree with subroot
+        # sameTree() will compare subtree with subroot
         # => our main() will RECURSIVELY pass in different subtree as a parameter
         
         # recursion
         # base case
         # if root is null and subroot is not: is not subtree
         # if root is not null and subroot is null: is subtree
-        # if root is null and subroot is null: pass to isMatch()
-        # if root is not null and subroot is not null: pass to isMatch()
+        # if root is null and subroot is null: pass to sameTree()
+        # if root is not null and subroot is not null: pass to sameTree()
         if root is None and subRoot is not None: return False
         if root is not None and subRoot is None: return True
         
-        if self.isMatch(root, subRoot): return True
+        if self.sameTree(root, subRoot): return True
         return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
         
         
         
-    def isMatch(self, root, subRoot):
+    def sameTree(self, root, subRoot):
         # recursion function to check if subTree match subRoot
         # base case
-        if (root is None and subRoot is not None) or (root is not None and subRoot is None): return False
+        # if they both null => Same tree
         if root is None and subRoot is None: return True
         
+        # if one of them is null => Not the same
+        if root is None or subRoot is None: return False
+        # if (root is None and subRoot is not None) or (root is not None and subRoot is None): return False
+        
         if root.val == subRoot.val: 
-            if self.isMatch(root.left, subRoot.left) and self.isMatch(root.right, subRoot.right):
+            if self.sameTree(root.left, subRoot.left) and self.sameTree(root.right, subRoot.right):
                 return True
-            else:
-                return False
+            
+        # if root.val != subRoot.val: return False
+        return False
         
