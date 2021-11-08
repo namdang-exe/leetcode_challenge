6af723252bae00647ed1f72951114e3b85d838bc
Nam Dang <dangquangnam75@gmail.com>
2021-11-08 11:54:15 -0600

    Find Smallest Letter Greater Than Target O(n)

---
+++ NextGreatestLetter.py
@@ -0,0 +1,7 @@
+# Brute force O(n)
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        for letter in letters:
+            if letter > target:
+                return letter
+        return letters[0]
