91ae6f0178a9fbab0781f59143172806da6e0369
Nam Dang <dangquangnam75@gmail.com>
2021-11-08 13:44:32 -0600

    Next Greatest Letter Binary Search O(logn)

--- NextGreatestLetter.py
+++ NextGreatestLetter.py
@@ -1,7 +1,18 @@
-# Brute force O(n)
+# bin search
 class Solution:
     def nextGreatestLetter(self, letters: List[str], target: str) -> str:
-        for letter in letters:
-            if letter > target:
-                return letter
-        return letters[0]
+        if target >= letters[-1]:
+            return letters[0]
+        low, high = 0, len(letters) - 1
+        while low < high:
+            mid = (low+high) // 2
+            # ignore the second half
+            if letters[mid] > target:
+                # why is this not mid - 1 ???
+                # Because if  "d" > "c" e.g
+                # d might be the result
+                high = mid
+            # ignore the first half
+            else:
+                low = mid + 1
+        return letters[low]
