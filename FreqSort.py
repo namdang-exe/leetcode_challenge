569020da59322f821fd7432b75b1e5e468940c66
Nam Dang <dangquangnam75@gmail.com>
2021-11-10 14:59:42 -0600

    Freq Sort, hashmap+heap, O(nlogn) time O(n) space

--- FreqSort.py
+++ FreqSort.py
@@ -1,12 +1,20 @@
-# Hashmap and Sort
-# O(nlogn) Time , O(n) space
+# Heap + hashmap
+# O(nlogn)
+from collections import Counter
+import heapq
 
 class Solution:
     def frequencySort(self, s: str) -> str:
+        # build hashmap for frequency
         cnt = Counter(s)
-        # t: 1, r: 1, e: 2
-        sorted_cnt = sorted(cnt.items(), key=lambda pairs:pairs[1], reverse=True)
+        
+        # build heap
+        heap = [(-v, k) for k,v in cnt.items()]
+        # min heap
+        heapq.heapify(heap)
+        # (-2, e), (-1, r), (-1, t)
         res = []
-        for key, val in sorted_cnt:
-            res += [key]*val
+        while heap:
+            v, k = heapq.heappop(heap)
+            res += [k] * -v
         return "".join(res)
