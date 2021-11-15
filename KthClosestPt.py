2729be848401501f503167f3f464444b514df778
Nam Dang <dangquangnam75@gmail.com>
2021-11-15 14:51:26 -0600

    Kth Closest Point O(nlogk) time

--- KthClosestPt.py
+++ KthClosestPt.py
@@ -1,21 +1,29 @@
-# First approach - min heap
-# O(klogn)
-#[(dist, [x,y])]
-# pop the heap k time
-# then return the value of the last pop
+# Second approach
+# Create a heap of size k
+# O(nlogk)
+import heapq
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         def eucli_dist(x,y):
-            return (x**2 + y**2)**0.5
-        heap = [(eucli_dist(x,y), [x,y]) for x,y in points]
-        # min heap
-        # O(n)
+            return (x**2+y**2)**0.5
+        
+        # [(dist, [x,y])]
+        heap = [(-eucli_dist(x,y), [x,y]) for x,y in points[:k]]
+        # build a max heap
+        # O(k)
         heapq.heapify(heap)
+
+        # add points to the heap
+        # O(nlogk)
+        for point in points[k:]:
+            x, y = point
+            if eucli_dist(x,y) < -heap[0][0]:
+                heapq.heapreplace(heap, (-eucli_dist(x,y), [x,y]))
+        # return the result array
+        # O(k)
         res = []
-        # pop kth closest point
-        # O(klogn)
-        for i in range(k):
-            val = heapq.heappop(heap)
+        for val in heap:
             pt = val[1]
             res.append(pt)
+        
         return res
