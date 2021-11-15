d77dc5790fd8113966026982b952029c0def80f4
Nam Dang <dangquangnam75@gmail.com>
2021-11-15 14:19:03 -0600

    Kth Largest Element O(1) time

--- KthLargestEle.py
+++ KthLargestEle.py
@@ -1,14 +1,29 @@
+# use min heap
+# pop all min element 
+# until we have k elements left in the array
+import heapq
 class KthLargest:
+
     def __init__(self, k: int, nums: List[int]):
         self.k = k
-        self.heap = nums
-        heapq.heapify(self.heap)
+        self.nums = nums
+        heapq.heapify(self.nums)
+        # this ensures we only have k largest elements
+        while len(self.nums) > k:
+            heapq.heappop(self.nums)
         
-        while len(self.heap) > k:
-            heapq.heappop(self.heap)
+        
 
     def add(self, val: int) -> int:
-        heapq.heappush(self.heap, val)
-        if len(self.heap) > self.k:
-            heapq.heappop(self.heap)
-        return self.heap[0]
+        heapq.heappush(self.nums, val)
+        # edge case 
+        # when self.nums is None and k = 1
+        # whatever we push is the largest number
+        if len(self.nums) > self.k:
+            heapq.heappop(self.nums)    
+        return self.nums[0]
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
