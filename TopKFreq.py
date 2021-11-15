7742092d569f950191a539e46d4004522d52e6af
Nam Dang <dangquangnam75@gmail.com>
2021-11-14 22:17:07 -0600

    Top K Freq O(nlogk)

--- TopKFreq.py
+++ TopKFreq.py
@@ -1,18 +1,24 @@
 # hashmap + heap
-# O(klogn)
+# O(n + 2k + nlogk) => O(nlogk)
 from collections import Counter
 import heapq
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # O(n)
         cnt = Counter(nums)
         n = len(nums)
         # build a heap
-        # max heap
-        heap = [(-v, k) for k, v in cnt.items()]
+        # min heap
+        heap = [(v,k) for k,v in list(cnt.items())[:k]]
+        # O(k)
         heapq.heapify(heap)
+        # add nums freq to heap
+        # O(nlogk)
+        for k, v in list(cnt.items())[k:]:
+            if v > heap[0][0]:
+                heapq.heapreplace(heap, (v,k))
         res = []
-        for i in range(k):
-            needed = heapq.heappop(heap)
-            res.append(needed[1])
-            
+        # O(k)
+        for i in range(len(heap))[::-1]:
+            res.append(heap[i][1])
         return res
