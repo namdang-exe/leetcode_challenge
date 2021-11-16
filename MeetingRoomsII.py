bc2ff4823cdac9c7d7634825f633a159ec16f1c4
Nam Dang <dangquangnam75@gmail.com>
2021-11-16 15:29:46 -0600

    Meeting Room II 2 arrays O(nlogn)

--- MeetingRoomsII.py
+++ MeetingRoomsII.py
@@ -1,32 +1,28 @@
-# Idea:
-# calculate how many meetings 
-# are going on at the same time
-# Tips: draw it out
-# check the start and stop of the meeting
-# if a meeting already started 
-# and then another meeting starts,
-# this means we have 2 meetings going at the same time
-# create a count to keep track
-# create a max count for how many meetings/ max room we need
-# for some meetings, the end time = the start time of other meeting
-# prioritize the end time
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        # [(time, start/stop)]
-        meetings = []
-        for start, stop in intervals:
-            # start, stop is time
-            meetings.append((start, "start"))
-            meetings.append((stop, "end"))
-        meetings.sort() # O(nlogn)
-        max_count = float('-inf')
-        count = 0
-        for meeting in meetings:
-            status = meeting[1]
-            if status == "start":
+        start = []
+        end = []
+        # O(n)
+        for s, e in intervals:
+            start.append(s)
+            end.append(e)
+        # O(nlogn)
+        start.sort()
+        end.sort()
+        # start: [0,5,15]
+        # end: [10,20,30]
+        res, count = 0, 0
+        i, j = 0, 0
+        # O(n)
+        while i < len(start):
+            # they are equal, we prioritize the end array
+            if start[i] < end[j]:
                 count += 1
+                i += 1
             else:
                 count -= 1
-            max_count = max(max_count, count)
+                j += 1
+            res = max(res, count)
             
-        return max_count
+        return res
+            
