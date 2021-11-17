28471041e369d3a02b62ef856969b68cb132a310
Nam Dang <dangquangnam75@gmail.com>
2021-11-17 14:18:51 -0600

    Fibonacci Tabulation O(n) time O(1) space

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,16 +1,15 @@
-# Tabulation aka build a table
-# Iterative
+# Tabulation without a table
+# O(n) time
+# O(1) space
 class Solution:
     def fib(self, n: int) -> int:
-        # initialize table
-        # [0,0,0,0,0,...]
+        if n == 0: return 0
+        if n == 1: return 1
         
-        if n == 0:
-            return 0
-        if n== 1:
-            return 1
-        tab = [0] * (n+1)
-        tab[1] = 1
-        for i in range(2,n+1):
-            tab[i] = tab[i-1] + tab[i-2]
-        return tab[n]
+        one, two = 0, 1
+        for i in range(n-1):
+            temp = two 
+            two = one + two 
+            one = temp 
+        
+        return two
