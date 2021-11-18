b02ddb4ff3ea0f256c8e53bda0001088310b1f10
Nam Dang <dangquangnam75@gmail.com>
2021-11-17 19:34:20 -0600

    Using - to Mark Location O(n) time O(1) s

--- FindDisappearNumbers.py
+++ FindDisappearNumbers.py
@@ -1,10 +1,15 @@
+# Using negative to mark location
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)):
+            # don't wanna mark the number again that will make it positive
+            #[-1,1,1] ==> 2,3 is the missing values
+            if nums[abs(nums[i]) - 1] > 0:
+                nums[abs(nums[i]) - 1] *= -1 
+        
         res = []
-        n = len(nums)
-        nums = set(nums)
-        for i in range(1, n +1):
-            if not i in nums:
-                res.append(i)
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                res.append(i+1)
                 
         return res
