cfa407b051b0d8dd4e6dd29c5ac0fa566491617e
Nam Dang <dangquangnam75@gmail.com>
2021-11-17 19:07:08 -0600

    Brute Force O(n^2) TLE

---
+++ FindDisappearNumbers.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        res = []
+        for i in range(1, len(nums)+1):
+            if not i in set(nums):
+                res.append(i)
+                
+        return res
