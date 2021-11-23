016df8be846a18ce7e829be299f7cb276e4fd66a
Nam Dang <dangquangnam75@gmail.com>
2021-11-22 20:06:35 -0600

    Greedy O(n) time O(1) space

--- JumpGame.py
+++ JumpGame.py
@@ -1,35 +1,11 @@
-# Recursion
+# Greedy algorithm
 class Solution:
-    def __init__(self):
-        # {3: false} -- this a dead end
-        self.cache = {}
-        
     def canJump(self, nums: List[int]) -> bool:
-        d = len(nums) - 1
-        return self.helper(nums, 0, d)
-    
-    def helper(self, nums, i, d):
-        """
-        nums: list of jump length
-        i: current pos
-        d: destination
-        """
-        
-        # Check cache
-        if i in self.cache: return False
-        
-        # Base case 
-        if i >= d: return True
-        if nums[i] == 0: return False
-        
-        # Recursion
-        jump_len = nums[i]
-        for l in range(1, jump_len+1)[::-1]:
-            idx = i + l
-            res = self.helper(nums, idx, d)
-            if res:
-                return True
-            else:
-                self.cache[idx] = res
-        return False
-            
+        n = len(nums)
+        goal = n -1
+        for i in range(n-1)[::-1]:
+            # Can I reach the goal from my current position?
+            # if yes, move the goal post to current position 
+            if nums[i] + i >= goal:
+                goal = i
+        return goal == 0
