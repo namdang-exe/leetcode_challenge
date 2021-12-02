d83bc241c88f2b83dbc76219130d019255f46929
Nam Dang <dangquangnam75@gmail.com>
2021-12-02 11:09:34 -0600

    Recursion Brute Force O(2^n) time

---
+++ BuyNSellStockCd.py
@@ -0,0 +1,32 @@
+# Recursion brute force
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # can_buy:
+        # True: can buy 
+        # False: sell
+        # buy => i = i + 1
+        # sell => i = i + 2
+        cache = {} # (i, can_buy)
+        
+        def dfs(i, can_buy):
+            # base cases
+            if i in cache: return cache[(i, can_buy)]
+            if i >= n: return 0
+            
+            # recursion
+            if can_buy:
+                buy = dfs(i+1, not can_buy) - prices[i]
+                cooldown = dfs(i+1, can_buy)
+                res = max(buy, cooldown)
+                cache[(i, can_buy)] = res
+            else:
+                sell = dfs(i + 2, True) + prices[i]
+                cooldown = dfs(i + 1, can_buy)
+                res = max(sell, cooldown)
+                cache[(i, can_buy)] = res
+                
+            return res
+        
+        n = len(prices)
+        return dfs(0, True)
