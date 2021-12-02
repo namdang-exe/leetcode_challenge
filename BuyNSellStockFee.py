e31c304fe85ead78a472d67dd8f9b6af28ea43ee
Nam Dang <dangquangnam75@gmail.com>
2021-12-02 15:53:20 -0600

    DP Tabulation O(n) time + space

--- BuyNSellStockFee.py
+++ BuyNSellStockFee.py
@@ -1,33 +1,20 @@
-# Recursion with Memoize
+# DP tabulation
+# bottom up 
 # O(n) time
 # O(n) space
-# Top down approach
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
-        n = len(prices)
-        cache = {}
-        # {(0, True): 1, (1, False): 2}
-        def dfs(i, can_buy):
-            "Recursive function"
-            # check cache
-            if (i, can_buy) in cache: return cache[(i,can_buy)]
-            
-            # base cases
-            if i >= n: return 0
-            
-            # recursion
-            price = prices[i]
-            if can_buy:
-                buy = dfs(i + 1, False) - price
-                hold = dfs(i + 1, True)
-                result = max(buy, hold)
-                cache[(i, can_buy)] = result
-            else:
-                sell = dfs(i+1, True) + price - fee
-                hold = dfs(i + 1, False)
-                result = max(sell, hold)
-                cache[(i, can_buy)] = result
-                
-            return cache[(i, can_buy)]
+        n = len(prices) + 1
+        sold = [0] * n
+        held = [0] * n
+        rest = [0] * n        
+        sold[0] = float('-inf')
+        held[0] = float('-inf')
         
-        return dfs(0, True)
+        for i in range(n-1):
+            price = prices[i]
+            sold[i + 1] = held[i] + price - fee
+            held[i + 1] = max(held[i], sold[i] - price, rest[i] - price)
+            rest[i + 1] = max(rest[i], sold[i])
+            
+        return max(sold[-1], rest[-1])
