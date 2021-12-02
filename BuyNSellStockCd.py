2de23da257ebc85fd94c3ba43a3c833f1cb99c97
Nam Dang <dangquangnam75@gmail.com>
2021-12-02 11:10:18 -0600

    Recursion w Memoize O(n) time + space

--- BuyNSellStockCd.py
+++ BuyNSellStockCd.py
@@ -1,4 +1,4 @@
-# Recursion brute force
+# Recursion with memoise
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
@@ -11,12 +11,12 @@ class Solution:
         
         def dfs(i, can_buy):
             # base cases
-            if i in cache: return cache[(i, can_buy)]
+            if (i, can_buy) in cache: return cache[(i, can_buy)]
             if i >= n: return 0
             
             # recursion
             if can_buy:
-                buy = dfs(i+1, not can_buy) - prices[i]
+                buy = dfs(i+1, False) - prices[i]
                 cooldown = dfs(i+1, can_buy)
                 res = max(buy, cooldown)
                 cache[(i, can_buy)] = res
@@ -26,7 +26,8 @@ class Solution:
                 res = max(sell, cooldown)
                 cache[(i, can_buy)] = res
                 
-            return res
+            return cache[(i, can_buy)]
         
         n = len(prices)
         return dfs(0, True)
+                
