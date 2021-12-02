e74a03f52dd023b138105affd142da854ae6d5f1
Nam Dang <dangquangnam75@gmail.com>
2021-12-02 11:10:40 -0600

    DP Tabulation O(n) time + space

--- BuyNSellStockCd.py
+++ BuyNSellStockCd.py
@@ -1,33 +1,19 @@
-# Recursion with memoise
+# DP with tabulation
+# O(n) time 
+# O(n) space
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # can_buy:
-        # True: can buy 
-        # False: sell
-        # buy => i = i + 1
-        # sell => i = i + 2
-        cache = {} # (i, can_buy)
-        
-        def dfs(i, can_buy):
-            # base cases
-            if (i, can_buy) in cache: return cache[(i, can_buy)]
-            if i >= n: return 0
+        n = len(prices) + 1
+        sold_dp = [0] * n
+        hold_dp = [0] * n
+        rest_dp = [0] * n
+        sold_dp[0] = float("-inf")
+        hold_dp[0] = float("-inf")
+        for i in range(n-1):
+            price = prices[i]
+            sold_dp[i+1] = hold_dp[i] + price
+            hold_dp[i+1] = max(hold_dp[i], rest_dp[i] - price) 
+            rest_dp[i+1] = max(rest_dp[i], sold_dp[i])
             
-            # recursion
-            if can_buy:
-                buy = dfs(i+1, False) - prices[i]
-                cooldown = dfs(i+1, can_buy)
-                res = max(buy, cooldown)
-                cache[(i, can_buy)] = res
-            else:
-                sell = dfs(i + 2, True) + prices[i]
-                cooldown = dfs(i + 1, can_buy)
-                res = max(sell, cooldown)
-                cache[(i, can_buy)] = res
-                
-            return cache[(i, can_buy)]
-        
-        n = len(prices)
-        return dfs(0, True)
-                
+        return max(sold_dp[-1], rest_dp[-1])
