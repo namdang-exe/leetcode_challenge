44357d8e5c500d70d2240e0a6331ca7908c6eab8
Nam Dang <dangquangnam75@gmail.com>
2021-12-02 16:07:05 -0600

    DP Tabulation 3 pointers O(n) time O(1) space

--- BuyNSellStockFee.py
+++ BuyNSellStockFee.py
@@ -1,20 +1,18 @@
 # DP tabulation
 # bottom up 
+# 3 pointers
 # O(n) time
-# O(n) space
+# O(1) space
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
-        n = len(prices) + 1
-        sold = [0] * n
-        held = [0] * n
-        rest = [0] * n        
-        sold[0] = float('-inf')
-        held[0] = float('-inf')
+        sold = float('-inf')
+        held = float('-inf')
+        rest = 0
         
-        for i in range(n-1):
-            price = prices[i]
-            sold[i + 1] = held[i] + price - fee
-            held[i + 1] = max(held[i], sold[i] - price, rest[i] - price)
-            rest[i + 1] = max(rest[i], sold[i])
+        for price in prices:
+            temp_sold = sold
+            sold = held + price - fee
+            held = max(held, temp_sold - price, rest - price)
+            rest = max(rest, temp_sold)
             
-        return max(sold[-1], rest[-1])
+        return max(sold, rest)
