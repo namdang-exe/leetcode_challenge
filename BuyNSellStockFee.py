3161cdbd94a35dec81f58ccca0623958c3e252a4
Nam Dang <dangquangnam75@gmail.com>
2021-12-02 15:52:59 -0600

    Recursion with Memoize O(n) time + space

--- BuyNSellStockFee.py
+++ BuyNSellStockFee.py
@@ -1,10 +1,17 @@
-# Recursion
-# O(2^n) time
+# Recursion with Memoize
+# O(n) time
+# O(n) space
+# Top down approach
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
         n = len(prices)
+        cache = {}
+        # {(0, True): 1, (1, False): 2}
         def dfs(i, can_buy):
-            # recursive function
+            "Recursive function"
+            # check cache
+            if (i, can_buy) in cache: return cache[(i,can_buy)]
+            
             # base cases
             if i >= n: return 0
             
@@ -14,11 +21,13 @@ class Solution:
                 buy = dfs(i + 1, False) - price
                 hold = dfs(i + 1, True)
                 result = max(buy, hold)
+                cache[(i, can_buy)] = result
             else:
                 sell = dfs(i+1, True) + price - fee
                 hold = dfs(i + 1, False)
                 result = max(sell, hold)
+                cache[(i, can_buy)] = result
                 
-            return result
+            return cache[(i, can_buy)]
         
         return dfs(0, True)
