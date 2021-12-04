9e5e3abab05632354c2158e1e054e7cce5c8fa75
Nam Dang <dangquangnam75@gmail.com>
2021-12-03 19:40:27 -0600

    Optimal Solution O(n^2) time

--- NumTeams.py
+++ NumTeams.py
@@ -1,12 +1,24 @@
-# Brute Force
-# O(n^3) time
+# More optimal solution
+# O(n^2) time
+
 class Solution:
     def numTeams(self, rating: List[int]) -> int:
         n = len(rating)
         team = 0
         for i in range(n):
-            for j in range(i, n):
-                for k in range(j, n):
-                    if rating[i] < rating[j] < rating[k] or rating[i] > rating[j] > rating[k]:
-                        team += 1
+            inc1, inc3, des1, des3 = 0,0,0,0
+            for j in range(n):
+                if j < i:
+                    if rating[j] > rating[i]:
+                        des1 += 1
+                    else:
+                        inc1 += 1
+                if j > i:
+                    if rating[j] > rating[i]:
+                        inc3 += 1
+                    else:
+                        des3 += 1
+            team += (des1*des3)
+            team += (inc1*inc3)
+            
         return team
