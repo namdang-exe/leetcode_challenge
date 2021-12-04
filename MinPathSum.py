3108e9a4216ac51765575fb8a9e08bffe4f6286e
Nam Dang <dangquangnam75@gmail.com>
2021-12-04 17:12:07 -0600

    1D DP tabulation O(mn) time O(n) space

--- MinPathSum.py
+++ MinPathSum.py
@@ -1,23 +1,19 @@
 # DP tabulation
 # Bottom up approach
+# 1D DP
 # O(mn) time
-# O(mn) space
+# O(n) space
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         m = len(grid)
         n = len(grid[0])
-        dp = [[0] * (n+1) for _ in range(m+1)]
-        # Make the out_of_bound grid = inf
-        for row_i in range(m):
-            dp[row_i][n] = float('inf')
-        for col_j in range(n):
-            dp[m][col_j] = float('inf')
-        dp[m][n-1] = 0
+        dp = [float('inf')] * (n+1)
+        dp[n-1] = 0
         
         # fill in dp
         for row_i in range(m)[::-1]:
             for col_j in range(n)[::-1]:
-                dp[row_i][col_j] = min(dp[row_i+1][col_j], dp[row_i][col_j+1]) + grid[row_i][col_j]
+                dp[col_j] = min(dp[col_j], dp[col_j + 1]) + grid[row_i][col_j]  
                 
-        return dp[0][0]
+        return dp[0]
     
