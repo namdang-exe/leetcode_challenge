90c41327b515701ed739ab66ff3fcaa77c785919
Nam Dang <dangquangnam75@gmail.com>
2021-12-05 14:12:25 -0600

    Inside out O(n^2) time O(1) space

--- LongestPalindrome.py
+++ LongestPalindrome.py
@@ -1,24 +1,26 @@
-# Brute force
+# Start in the middle and branching out method
+
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        def check_palindrome(s):
-            l, r = 0, len(s) - 1
-            while l < r:
-                if s[l] != s[r]:
-                    return False
-                l += 1
-                r -= 1
-            return True
-        
-        
         n = len(s)
         max_len = 0
         res = None
         for i in range(n):
-            for j in range(i, n):
-                if check_palindrome(s[i:j+1]):
-                    if max_len < len(s[i:j+1]):
-                        max_len = len(s[i:j+1])
-                        res = s[i:j+1]
-                        
+            l, r = i, i
+            # odd substring
+            while l >= 0 and r < n and s[l] == s[r]:
+                if max_len < r - l + 1 :
+                    max_len = r - l + 1
+                    res = s[l:r+1]
+                l -= 1
+                r += 1
+            # even substring
+            l,r = i, i+1
+            while l >= 0 and r < n and s[l] == s[r]:
+                if max_len < r - l + 1 :
+                    max_len = r - l + 1
+                    res = s[l:r+1]
+                l -= 1
+                r += 1
         return res
+                
