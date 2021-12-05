165c3ec8babb35a4b8c848649d2ba21ec3313b82
Nam Dang <dangquangnam75@gmail.com>
2021-12-04 20:14:41 -0600

    DP max_left, max_right, max_sum O(n) time + space

--- MaxSubarraySumWDeletion.py
+++ MaxSubarraySumWDeletion.py
@@ -4,22 +4,19 @@ class Solution:
         n = len(arr)
         
         max_left, max_right, max_sum = [0] * n, [0] * n, [0] * n
-        max_right += [0]
         max_left[0], max_sum[0] = arr[0], arr[0]
+        max_right[-1] = arr[-1]
         
-        # max_left dp
+        # max_left, max_right, max_sum dp
         for i in range(1,n):
-            num = arr[i]
-            max_left[i] = max(max_left[i-1]+num, num)
+            max_left[i] = max(max_left[i-1]+arr[i], arr[i])
             max_sum[i] = max(max_sum[i-1], max_left[i])
-        # max_right dp
-        for i in range(n)[::-1]:
-            num = arr[i]
-            max_right[i] = max(max_right[i+1]+num, num)
+            max_right[-i-1] = max(max_right[-i]+arr[-i-1], arr[-i-1])
+
+        max_right += [0]
         # res = max(max_left + max_right - num)
         res = arr[0]
         for i in range(1, n):
             res = max(res, max_left[i-1] + max_right[i+1], max_sum[i])
             
         return res
-        
