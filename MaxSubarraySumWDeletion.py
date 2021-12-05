6a82c94fdc9539a4ad7ab92ab069e1d50d063773
Nam Dang <dangquangnam75@gmail.com>
2021-12-04 20:00:50 -0600

    Kadane left and right O(n) time + space clean up

--- MaxSubarraySumWDeletion.py
+++ MaxSubarraySumWDeletion.py
@@ -2,28 +2,24 @@
 class Solution:
     def maximumSum(self, arr: List[int]) -> int:
         n = len(arr)
-        # edge case
-        if n == 1: return arr[-1]
         
-        max_left, max_right = [0] * (n+2), [0] * (n+2)
-        max_left[0], max_right[-1] = 0,0 
-        max_left[1], max_right[-2] = arr[0], arr[-1]
-        max_sum = [0] * n
-        max_sum[0] = arr[0]
+        max_left, max_right, max_sum = [0] * n, [0] * n, [0] * n
+        max_right += [0]
+        max_left[0], max_sum[0] = arr[0], arr[0]
         
         # max_left dp
         for i in range(1,n):
             num = arr[i]
-            max_left[i+1] = max(max_left[i]+num, num)
-            max_sum[i] = max(max_sum[i-1], max_left[i+1])
+            max_left[i] = max(max_left[i-1]+num, num)
+            max_sum[i] = max(max_sum[i-1], max_left[i])
         # max_right dp
         for i in range(n)[::-1]:
             num = arr[i]
-            max_right[i+1] = max(max_right[i+2]+num, num)
+            max_right[i] = max(max_right[i+1]+num, num)
         # res = max(max_left + max_right - num)
-        res = float('-inf')
-        for i in range(n):
-            res = max(res, max_left[i] + max_right[i+2], max_sum[i])
+        res = arr[0]
+        for i in range(1, n):
+            res = max(res, max_left[i-1] + max_right[i+1], max_sum[i])
             
         return res
         
