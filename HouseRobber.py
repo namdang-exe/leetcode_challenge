52c05902775b0cce00fc49cd37521ad316aab7bb
Nam Dang <dangquangnam75@gmail.com>
2021-12-05 19:53:00 -0600

    Recursion with Memoization O(n) time + space

--- HouseRobber.py
+++ HouseRobber.py
@@ -1,18 +1,19 @@
-# Brute force recursion
-# O(2^n) time
-# O(1) space
+# Recursion with Memoization
+# O(n) time
+# O(n) space
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
-        if n == 2: return max(nums)
-        
+        cache = {}
+        # {3:7}
         def dfs(i):
             # dfs recursion
+            if i in cache: return cache[i]
             # base case
             if i >= n: return 0
             if n - i == 1: return nums[i]
             
             res = max(nums[i] + dfs(i+2), nums[i+1] + dfs(i+3))
-            return res
+            cache[i] = res
+            return cache[i]
         
-        return dfs(0)
