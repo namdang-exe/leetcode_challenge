97bf4de28bbc2f1b4379e5e24c02ec465c9fee60
Nam Dang <dangquangnam75@gmail.com>
2021-12-06 11:42:37 -0600

    Heapq push + pop O(nlogn) time O(n) space

--- HighFive.py
+++ HighFive.py
@@ -1,17 +1,12 @@
-# use hashmap to store all scores of IDs
-# scores are stored in a list []
-# sort list []
-# print the average of top 5
-# Time complexity: O(n) + O(i * slog(s))
-
 class Solution:
     def highFive(self, items: List[List[int]]) -> List[List[int]]:
-        db = dict() # {ID:[]} {1:[100,85]}
-        for ID, score in items: # O
-            db[ID] = db.get(ID, []) + [score]
-        res = []
-        for k in db.keys(): # O(i)
-            top_five = sorted(db[k], reverse=True)[:5]
-            avg = sum(top_five)//5
-            res.append([k, avg])
-        return sorted(res, key=lambda pairs:pairs[0])
+        d = collections.defaultdict(list)
+        for idx, val in items:
+            heapq.heappush(d[idx], val)
+            
+            if len(d[idx]) > 5:
+                heapq.heappop(d[idx])
+        
+        res = [[i, sum(d[i]) // len(d[i])] for i in sorted(d)]
+        
+        return res
