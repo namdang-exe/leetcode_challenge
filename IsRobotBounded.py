841f9f68c92c5813c2acf256ed83f2c3dc39e1e9
Nam Dang <dangquangnam75@gmail.com>
2021-12-06 10:30:06 -0600

    Change Pos, Dir Linear Algebra O(n) time

--- IsRobotBounded.py
+++ IsRobotBounded.py
@@ -1,37 +1,19 @@
 class Solution:
     def isRobotBounded(self, instructions: str) -> bool:
-        def change_pos(pos, cur_dir):
-            if cur_dir == 0: pos[1] += 1
-            elif cur_dir == 1: pos[0] += 1
-            elif cur_dir == 2: pos[1] -= 1
-            else: pos[0] -= 1
-            return pos
-        
-        def change_dir(cur_dir, d):
-            if d == "L":
-                if cur_dir == 0:
-                    cur_dir = 3
-                else:
-                    cur_dir -= 1
-            if d == "R":
-                if cur_dir == 3:
-                    cur_dir = 0
-                else:
-                    cur_dir += 1
-            return cur_dir
-        
-        cur_pos = [0,0]  # (x,y)
-        cur_dir = 0
+        x,y = 0,0
+        dx,dy = 0, 1
         # carry out instructions
-        for i in instructions:
-            if i == "G":
-                cur_pos = change_pos(cur_pos, cur_dir)
+        for d in instructions:
+            if d == "G":
+                x, y = x + dx, y + dy
+            elif d == "L":
+                dx, dy = -dy, dx
             else:
-                cur_dir = change_dir(cur_dir, i)
+                dx, dy = dy, -dx
         # check if it's a cycle
-        if cur_pos == [0,0]:
+        if x == 0 and y == 0:
             return True
         else:
-            if cur_dir != 0:
+            if dx != 0 or dy != 1:
                 return True
         return False
