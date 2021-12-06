f6cfceec93c94ea00e50f3082074d9f55048f286
Nam Dang <dangquangnam75@gmail.com>
2021-12-06 15:45:58 -0600

    O(n) time O(1) space

---
+++ RobotReturnOrigin.py
@@ -0,0 +1,14 @@
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        x, y = 0, 0
+        for m in moves:
+            if m == "R":
+                x += 1
+            elif m == "L":
+                x -= 1
+            elif m == "U":
+                y += 1
+            else:
+                y -= 1
+                
+        return (x,y) == (0,0)
