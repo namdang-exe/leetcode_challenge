d1f44e8e8451e3674d27d2d5b524e46efcc6913e
Nam Dang <dangquangnam75@gmail.com>
2021-12-05 20:01:49 -0600

    DP Tabulation O(n) time + space

--- HouseRobber.py
+++ HouseRobber.py
@@ -1,19 +1,16 @@
-# Recursion with Memoization
+# DP tabulation
+# Bottom up approach
 # O(n) time
 # O(n) space
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
-        cache = {}
-        # {3:7}
-        def dfs(i):
-            # dfs recursion
-            if i in cache: return cache[i]
-            # base case
-            if i >= n: return 0
-            if n - i == 1: return nums[i]
-            
-            res = max(nums[i] + dfs(i+2), nums[i+1] + dfs(i+3))
-            cache[i] = res
-            return cache[i]
+        if n == 1: return nums[-1]
+        dp = [0] * n
+        dp[0] = nums[0]
+        dp[1] = max(dp[0], nums[1])
         
+        for i in range(2,  n):
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+            
+        return dp[-1]
