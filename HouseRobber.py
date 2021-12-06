d103b74b3caa9b22e5d670cb3449b7b4cc94a87c
Nam Dang <dangquangnam75@gmail.com>
2021-12-05 20:06:15 -0600

    DP Tabulation 2 pointers O(n) time O(1) space

--- HouseRobber.py
+++ HouseRobber.py
@@ -1,16 +1,14 @@
 # DP tabulation
 # Bottom up approach
+# 2 pointers 
 # O(n) time
-# O(n) space
+# O(1) space
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
-        if n == 1: return nums[-1]
-        dp = [0] * n
-        dp[0] = nums[0]
-        dp[1] = max(dp[0], nums[1])
-        
-        for i in range(2,  n):
-            dp[i] = max(dp[i-1], dp[i-2] + nums[i])
-            
-        return dp[-1]
+        one, two = 0, 0
+        for i in range(n):
+            temp = two
+            two = max(nums[i]+one, two)
+            one = temp
+        return two
