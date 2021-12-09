6571533be66398179c7c421c71d7937af47c20f8
Nam Dang <dangquangnam75@gmail.com>
2021-12-09 09:31:45 -0600

    Hashmap O(n) time O(n) space

---
+++ FindDuplicates.py
@@ -0,0 +1,14 @@
+# hashmap
+# O(n) time
+# O(n) space
+from collections import Counter
+
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        cnt = Counter(nums)
+        res = []
+        for k, v in cnt.items():
+            if v > 1:
+                res.append(k)
+                
+        return res
